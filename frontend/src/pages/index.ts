0887166218f7c4bf8c571b25a61ecc4df3eb3987
Samuel Ogboye <ogboyesam@gmail.com>
2026-01-19 22:31:49 +0100

    feat: create index file to export public, auth, student, instructor, and shared pages

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,17 @@
+// Public pages
+export { default as Landing } from './public/Landing'
+export { default as About } from './public/About'
+export { default as Contact } from './public/Contact'
+export { default as NotFound } from './public/NotFound'
+
+// Auth pages
+export * from './auth'
+
+// Student pages
+export * from './student'
+
+// Instructor pages
+export * from './instructor'
+
+// Shared pages
+export * from './shared'
